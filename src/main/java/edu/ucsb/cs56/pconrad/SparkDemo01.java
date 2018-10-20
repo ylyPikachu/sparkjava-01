d43a3ff89228730bcc6b12a18e75892e86d31f33
ylyPikachu <43805174+ylyPikachu@users.noreply.github.com>
2018-10-19 18:07:11 -0700

    Update SparkDemo01.java

--- src/main/java/edu/ucsb/cs56/pconrad/SparkDemo01.java
+++ src/main/java/edu/ucsb/cs56/pconrad/SparkDemo01.java
@@ -11,27 +11,27 @@ import static spark.Spark.port;
 public class SparkDemo01 {
 
     public static void main(String[] args) {
+	    
 	    	String html = "<h1><a href='/hello'>Hello</a> World!</h1>\n" +
     "<p>This web app is powered by \n" +
-    "<a href='https://github.com/ylyPikachu/sparkjava-01'>this github repo</a></p>\n";
-	    
+    "<a href='https://github.com/zcy4014/sparkjava-01'>this github repo</a></p>\n";
 
 		System.out.println("About to set the port...");
-		
-        port(getHerokuAssignedPort());
+		port(getHerokuAssignedPort());
 		
 		System.out.println("");
 		System.out.println("(Don't worry about the warnings below about SLF4J... we'll deal with those later)");
 		System.out.println("");						  
 		System.out.println("In browser, visit: http://localhost:" + getHerokuAssignedPort() + "/hello");
 		System.out.println("");
-		
-	    	spark.Spark.get("/hello", (req, res) -> "<b>Hello World!</b>\n");
+		//spark.Spark.get("/hello", (req, res) -> "<b>Hello World!</b>\n");
 
 		spark.Spark.get("/nihao", (req, res) -> "<b>Ni Hao</b>\n");
 
 		spark.Spark.get("/hola", (req, res) -> "<b>¡Hola!</b>\n");
 		spark.Spark.get("/", (req, res) -> html);
+	    
+	    	spark.Spark.get("/hello", (req, res) -> "<p><b>Hello, World!</b>  You just clicked the first link on my web app.</p>");
 
 	}
 	
